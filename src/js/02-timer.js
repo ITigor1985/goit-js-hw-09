fdd32fcb012b509f2f9c86f7527b9cae27f0c1f9
Igor Tarasov <xromer85@gmail.com>
2021-11-06 23:35:42 +0300

    выловил время

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,22 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const inputeEl = document.querySelector("input[type=text]");
+const inputeEl = document.querySelector('input[type=text]');
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-flatpickr("#datetime-picker", options);
-console.log(inputeEl)
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const endTime = selectedDates[0].getTime();
+    const startTime = options.defaultDate.getTime();
+    if (startTime > endTime) {
+      alert('Please choose a date in the future');
+    }
+    console.log(startTime);
+    console.log(endTime);
+  },
+};
+const fp = flatpickr('#datetime-picker', options);
+
+//console.log(fp.selectedDates);
